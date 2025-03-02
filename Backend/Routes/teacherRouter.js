2a186ebc101222fbae1e0939b83233a762c4e400
Akash Jha <akashxjha@gmail.com>
2025-03-02 16:26:44 +0530

    Commite by ajasg

--- Backend/Routes/teacherRouter.js
+++ Backend/Routes/teacherRouter.js
@@ -1,9 +1,7 @@
 import express from 'express'
-<<<<<<< HEAD
+
 import { loginTeacher, signupTeacher } from '../Controller/teacherController';
-=======
-import { loginTeacher, signupTeacher } from '../Controller/teacherController.js';
->>>>>>> 1651085 (Commite by ajasg)
+
 const teacherRouter =express.Router();
 teacherRouter.use('/sign-up',signupTeacher);
 teacherRouter.use('/login',loginTeacher);
