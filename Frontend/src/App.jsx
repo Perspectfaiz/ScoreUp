931571a23837ff759d6888043f71677b6343155b
Akash Jha <akashxjha@gmail.com>
2025-03-03 16:24:34 +0530

    App improved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components/navbar'
+import { Alert } from './components/Alert'
+import { Card } from './components/card'
+import { Hero } from './components/Hero'
+import { Explore } from './components/explore'
+import { Footer } from './components/footer'
+import { Studentprofile } from './components/studentprofile'
+
+
 import Login from './pages/Login'
 import { ToastContainer, toast } from 'react-toastify'; 
 function App() {
@@ -11,8 +21,24 @@ function App() {
     <>
     <ToastContainer />
      <Login></Login>
+    
+      
+      <Alert></Alert>
+      <Hero></Hero>
+      <Explore></Explore>
+      
+      <Navbar></Navbar>
+      <Studentprofile></Studentprofile>
+   
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
