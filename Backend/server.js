9f391e5f361a211569849b5f35ddc6179fa0f3d3
Manish-verma40 <156562305+Manish-verma40@users.noreply.github.com>
2025-03-05 22:06:32 +0530

    maniya boss

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import connectDB from './Config/MongoDB.js';
 import express from 'express';
 import studentRouter from './Routes/studentRouter.js';
 import teacherRouter from './Routes/teacherRouter.js';
+import adminRouter from './Routes/adminRouter.js';
  const port =8080;
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cors());
 //localhost:8080/api/student/...
 app.use('/api/student',studentRouter);
 app.use('/api/teacher',teacherRouter);
-
+app.use('/api/admin',adminRouter);
 app.listen(8080,()=>{
 console.log(`app is listening at port ${port}`);
 })
