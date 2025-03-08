a38a867bcce271cd205e9daa0e4bf73997827f4e
Akash Jha <akashxjha@gmail.com>
2025-03-08 15:27:12 +0530

    Api of test

--- Backend/Routes/teacherRouter.js
+++ Backend/Routes/teacherRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { loginTeacher, signupTeacher,extractText } from '../Controller/teacherController.js';
+import { loginTeacher, signupTeacher, createTest } from '../Controller/teacherController.js';
 
 const teacherRouter =express.Router();
 teacherRouter.use('/sign-up',signupTeacher);
 teacherRouter.use('/login',loginTeacher);
-teacherRouter.use('/text',extractText);
-studentRouter.use('/profile',updateTeacherProfile)
+// teacherRouter.use('/text',extractText);
+// teacherRouter.use('/profile',updateTeacherProfile)
+teacherRouter.use('/create-test',createTest);
 export default teacherRouter;
