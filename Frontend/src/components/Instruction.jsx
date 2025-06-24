f5d391b8e58454f7a71dcfeb2386155f6bab6504
Akash Jha <akashxjha@gmail.com>
2025-06-24 14:35:44 +0530

    error solved

--- Frontend/src/components/Instruction.jsx
+++ Frontend/src/components/Instruction.jsx
@@ -25,44 +25,44 @@ export function Instruction({hide}) {
                         <ul>
                         <li>The test comprises [Number] questions, divided into various sections.</li>
                         <li>Each section may includes a variety of question types:
-                            <ul>
-                            <li>Multiple Choice Questions (MCQs) with a single correct answer.</li>
-                            <li>Multiple Choice Questions (MCQs) with multiple correct answers.</li>
-                            <li>Numerical Value Questions (NVQs).</li>
-                            <li>Assertion and Reason.</li>
-                            <li>Match the following.</li>
-                            </ul>
-                            <li>Instructions for each question type will be provided alongside the questions.</li>
+                            <div style={{marginLeft: '20px'}}>
+                                <div>• Multiple Choice Questions (MCQs) with a single correct answer.</div>
+                                <div>• Multiple Choice Questions (MCQs) with multiple correct answers.</div>
+                                <div>• Numerical Value Questions (NVQs).</div>
+                                <div>• Assertion and Reason.</div>
+                                <div>• Match the following.</div>
+                            </div>
+                            <div style={{marginLeft: '20px'}}>Instructions for each question type will be provided alongside the questions.</div>
                         </li>
                         </ul>
 
                         <p><strong>Marking Scheme:</strong></p>
                         <ul>
                         <li><strong>MCQs (Single Correct):</strong>
-                            <ul>
-                            <li>+4 marks for a correct answer.</li>
-                            <li>-1 mark for an incorrect answer.</li>
-                            <li>0 marks for unanswered questions.</li>
-                            </ul>
+                            <div style={{marginLeft: '20px'}}>
+                                <div>• +4 marks for a correct answer.</div>
+                                <div>• -1 mark for an incorrect answer.</div>
+                                <div>• 0 marks for unanswered questions.</div>
+                            </div>
                         </li>
                         <li><strong>MCQs (Multiple Correct):</strong>
-                            <ul>
-                            <li>+4 marks for all correct options selected.</li>
-                            <li>Partial marks may be awarded if some, but not all, correct options are selected.</li>
-                            <li>Negative marks for incorrect options selected.</li>
-                            <li>0 marks for unanswered questions.</li>
-                            </ul>
+                            <div style={{marginLeft: '20px'}}>
+                                <div>• +4 marks for all correct options selected.</div>
+                                <div>• Partial marks may be awarded if some, but not all, correct options are selected.</div>
+                                <div>• Negative marks for incorrect options selected.</div>
+                                <div>• 0 marks for unanswered questions.</div>
+                            </div>
                         </li>
                         <li><strong>Numerical Value Questions (NVQs):</strong>
-                            <ul>
-                            <li>+4 marks for a correct answer.</li>
-                            <li>0 marks for incorrect or unanswered questions.</li>
-                            </ul>
+                            <div style={{marginLeft: '20px'}}>
+                                <div>• +4 marks for a correct answer.</div>
+                                <div>• 0 marks for incorrect or unanswered questions.</div>
+                            </div>
                         </li>
                         <li><strong>Assertion and Reason and Match the following:</strong>
-                            <ul>
-                            <li>Follow the specific instructions given within those question sets for their marking scheme.</li>
-                            </ul>
+                            <div style={{marginLeft: '20px'}}>
+                                <div>• Follow the specific instructions given within those question sets for their marking scheme.</div>
+                            </div>
                         </li>
                         </ul>
 
@@ -71,14 +71,14 @@ export function Instruction({hide}) {
                         <li>Use the navigation buttons ("Next," "Previous") to move between questions.</li>
                         <li>A "Question Palette" will display all question numbers, allowing you to jump to specific questions.</li>
                         <li>The "question palette" interface displays different icons to help navigating through the test efficiently. Each icon has a specific meaning:
-                            <ul>
-                                <li className={styles.liner}><Qnbtn num={1} btn={0}></Qnbtn> Not Visited (The question has not been viewed yet.)</li>
-                                <li className={styles.liner}><Qnbtn num={1} btn={1}></Qnbtn> Not Answered (The question has been viewed but not answered.)</li>
-                                <li className={styles.liner}><Qnbtn num={1} btn={2}></Qnbtn> Answered (The question has been answered and saved.)</li>
-                                <li className={styles.liner}><Qnbtn num={1} btn={3}></Qnbtn> Current Question (The question you are currently viewing.)</li>
-                                <li className={styles.liner}><Qnbtn num={1} btn={4}></Qnbtn> Marked for Review & Not Answered (The question has been marked for review but has not been answered. It won’t be considered in evaluation unless answered later.)</li>
-                                <li className={styles.liner}><Qnbtn num={1} btn={5}></Qnbtn> Marked for Review & Answered (The question has been answered, but you have marked it for review. The answer will be considered in evaluation.)</li>
-                            </ul>
+                            <div style={{marginLeft: '20px'}}>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 0}}></Qnbtn> Not Visited (The question has not been viewed yet.)</div>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 1}}></Qnbtn> Not Answered (The question has been viewed but not answered.)</div>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 2}}></Qnbtn> Answered (The question has been answered and saved.)</div>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 3}}></Qnbtn> Current Question (The question you are currently viewing.)</div>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 4}}></Qnbtn> Marked for Review & Not Answered (The question has been marked for review but has not been answered. It won't be considered in evaluation unless answered later.)</div>
+                                <div className={styles.liner}><Qnbtn num={1} qdata={{state: 5}}></Qnbtn> Marked for Review & Answered (The question has been answered, but you have marked it for review. The answer will be considered in evaluation.)</div>
+                            </div>
                         </li>
                         </ul>
 
